2c2bdb278f104c973506e62f2056ed2b4d14d2c2
rob tillaart <rob.tillaart@gmail.com>
2020-12-07 11:28:35 +0100

    initial arduino-ci + unit test + some refactor

--- PCF8574.cpp
+++ PCF8574.cpp
@@ -2,12 +2,13 @@
 //    FILE: PCF8574.cpp
 //  AUTHOR: Rob Tillaart
 //    DATE: 02-febr-2013
-// VERSION: 0.2.1
+// VERSION: 0.2.2
 // PURPOSE: Arduino library for PCF8574 - I2C IO expander
 //     URL: https://github.com/RobTillaart/PCF8574
 //          http://forum.arduino.cc/index.php?topic=184800
 //
 // HISTORY:
+// 0.2.2  2020-12-07 add Arduino-ci + start unit test + Wire.h in PCF8574.h
 // 0.2.1  2020-06-19 fix library.json
 // 0.2.0  2020-05-22 #pragma once; refactor;
 //        removed pre 1.0 support
@@ -40,16 +41,14 @@
 
 #include "PCF8574.h"
 
-#include <Wire.h>
-
 
 PCF8574::PCF8574(const uint8_t deviceAddress)
 {
-    _address = deviceAddress;
-    _dataIn = 0;
-    _dataOut = 0xFF;
-    _buttonMask = 0xFF;
-    _error = PCF8574_OK;
+  _address = deviceAddress;
+  _dataIn = 0;
+  _dataOut = 0xFF;
+  _buttonMask = 0xFF;
+  _error = PCF8574_OK;
 }
 
 #if defined (ESP8266) || defined(ESP32)
@@ -73,102 +72,103 @@ void PCF8574::begin(uint8_t val)
 // TODO @800KHz -> ??
 uint8_t PCF8574::read8()
 {
-    if (Wire.requestFrom(_address, (uint8_t)1) != 1)
-    {
-        _error = PCF8574_I2C_ERROR;
-        return _dataIn; // last value
-    }
-    _dataIn = Wire.read();
-    return _dataIn;
+  if (Wire.requestFrom(_address, (uint8_t)1) != 1)
+  {
+    _error = PCF8574_I2C_ERROR;
+    return _dataIn; // last value
+  }
+  _dataIn = Wire.read();
+  return _dataIn;
 }
 
 void PCF8574::write8(const uint8_t value)
 {
-    _dataOut = value;
-    Wire.beginTransmission(_address);
-    Wire.write(_dataOut);
-    _error = Wire.endTransmission();
+  _dataOut = value;
+  Wire.beginTransmission(_address);
+  Wire.write(_dataOut);
+  _error = Wire.endTransmission();
 }
 
 uint8_t PCF8574::read(const uint8_t pin)
 {
-    if (pin > 7)
-    {
-        _error = PCF8574_PIN_ERROR;
-        return 0;
-    }
-    PCF8574::read8();
-    return (_dataIn & (1 << pin)) > 0;
+  if (pin > 7)
+  {
+    _error = PCF8574_PIN_ERROR;
+    return 0;
+  }
+  PCF8574::read8();
+  return (_dataIn & (1 << pin)) > 0;
 }
 
 void PCF8574::write(const uint8_t pin, const uint8_t value)
 {
-    if (pin > 7)
-    {
-        _error = PCF8574_PIN_ERROR;
-        return;
-    }
-    if (value == LOW)
-    {
-        _dataOut &= ~(1 << pin);
-    }
-    else
-    {
-        _dataOut |= (1 << pin);
-    }
-    write8(_dataOut);
+  if (pin > 7)
+  {
+    _error = PCF8574_PIN_ERROR;
+    return;
+  }
+  if (value == LOW)
+  {
+    _dataOut &= ~(1 << pin);
+  }
+  else
+  {
+    _dataOut |= (1 << pin);
+  }
+  write8(_dataOut);
 }
 
 void PCF8574::toggle(const uint8_t pin)
 {
-    if (pin > 7)
-    {
-        _error = PCF8574_PIN_ERROR;
-        return;
-    }
-    toggleMask(1 << pin);
+  if (pin > 7)
+  {
+    _error = PCF8574_PIN_ERROR;
+      return;
+  }
+  toggleMask(1 << pin);
 }
 
 void PCF8574::toggleMask(const uint8_t mask)
 {
-    _dataOut ^= mask;
-    PCF8574::write8(_dataOut);
+  _dataOut ^= mask;
+  PCF8574::write8(_dataOut);
 }
 
 void PCF8574::shiftRight(const uint8_t n)
 {
-	if ((n == 0) || (_dataOut == 0)) return;
-	if (n > 7)         _dataOut = 0;    // shift 8++ clears all, valid...
-	if (_dataOut != 0) _dataOut >>= n;
-    PCF8574::write8(_dataOut);
+  if ((n == 0) || (_dataOut == 0)) return;
+  if (n > 7)         _dataOut = 0;    // shift 8++ clears all, valid...
+  if (_dataOut != 0) _dataOut >>= n;
+  PCF8574::write8(_dataOut);
 }
 
 void PCF8574::shiftLeft(const uint8_t n)
 {
-	if ((n == 0) || (_dataOut == 0)) return;
-	if (n > 7)         _dataOut = 0;    // shift 8++ clears all, valid...
-	if (_dataOut != 0) _dataOut <<= n;  // only shift if there are bits set
-    PCF8574::write8(_dataOut);
+  if ((n == 0) || (_dataOut == 0)) return;
+  if (n > 7)         _dataOut = 0;    // shift 8++ clears all, valid...
+  if (_dataOut != 0) _dataOut <<= n;  // only shift if there are bits set
+  PCF8574::write8(_dataOut);
 }
 
 int PCF8574::lastError()
 {
-    int e = _error;
-    _error = PCF8574_OK;
-    return e;
+  int e = _error;
+  _error = PCF8574_OK;
+  return e;
 }
 
 void PCF8574::rotateRight(const uint8_t n)
 {
-	if ((n % 8) == 0) return;
-    uint8_t r = n & 7;
-    _dataOut = (_dataOut >> r) | (_dataOut << (8 - r));
-    PCF8574::write8(_dataOut);
+  if ((n % 8) == 0) return;
+
+  uint8_t r = n & 7;
+  _dataOut = (_dataOut >> r) | (_dataOut << (8 - r));
+  PCF8574::write8(_dataOut);
 }
 
 void PCF8574::rotateLeft(const uint8_t n)
 {
-    rotateRight(8- (n & 7));
+  rotateRight(8- (n & 7));
 }
 
 void PCF8574::reverse() // quite fast: 14 shifts, 3 or, 3 assignment.
@@ -183,26 +183,29 @@ void PCF8574::reverse() // quite fast: 14 shifts, 3 or, 3 assignment.
 //added 0.1.07/08 Septillion
 uint8_t PCF8574::readButton8(const uint8_t mask)
 {
-    uint8_t temp = _dataOut;
-    PCF8574::write8(mask | _dataOut); 
-    PCF8574::read8();
-    PCF8574::write8(temp);
-    return _dataIn;
+  uint8_t temp = _dataOut;
+  PCF8574::write8(mask | _dataOut); 
+  PCF8574::read8();
+  PCF8574::write8(temp);
+
+  return _dataIn;
 }
 
 //added 0.1.07 Septillion
 uint8_t PCF8574::readButton(const uint8_t pin)
 {
-    if (pin > 7)
-    {
-        _error = PCF8574_PIN_ERROR;
-        return 0;
-    }
-    uint8_t temp = _dataOut;
-    PCF8574::write(pin, HIGH);
-    uint8_t rtn = PCF8574::read(pin);
-    PCF8574::write8(temp);
-    return rtn;
+  if (pin > 7)
+  {
+    _error = PCF8574_PIN_ERROR;
+    return 0;
+  }
+
+  uint8_t temp = _dataOut;
+  PCF8574::write(pin, HIGH);
+  uint8_t rtn = PCF8574::read(pin);
+  PCF8574::write8(temp);
+
+  return rtn;
 }
 
 // -- END OF FILE --
---
+++ test/unit_test_001.cpp
@@ -0,0 +1,73 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.0
+//    DATE: 2020-12-03
+// PURPOSE: unit tests for the PCF8574 I2C port expander
+//          https://github.com/RobTillaart/PCF8574
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// ----------------------------
+// assertEqual(expected, actual)
+// assertNotEqual(expected, actual)
+// assertLess(expected, actual)
+// assertMore(expected, actual)
+// assertLessOrEqual(expected, actual)
+// assertMoreOrEqual(expected, actual)
+// assertTrue(actual)
+// assertFalse(actual)
+// assertNull(actual)
+
+#include <ArduinoUnitTests.h>
+
+#include "Arduino.h"
+#include "PCF8574.h"
+
+PCF8574 PCF(0x38);
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+unittest(test_begin)
+{
+  PCF8574 PCF(0x38);
+
+  PCF.begin();
+  int r = PCF.read8();
+  assertEqual(0, r);
+
+  int expect = PCF8574_I2C_ERROR;
+  assertEqual(expect, PCF.lastError());
+}
+
+unittest(test_read)
+{
+  PCF8574 PCF(0x38);
+  int r;
+
+  PCF.begin();
+  for (int i = 0; i < 8; i++)
+  {
+    r = PCF.read(i);
+    assertEqual(0, r);
+
+    int expect = PCF8574_I2C_ERROR;
+    assertEqual(expect, PCF.lastError());
+  }
+  r = PCF.read(8);
+  assertEqual(0, r);
+  int expect = PCF8574_PIN_ERROR;
+  assertEqual(expect, PCF.lastError());
+}
+
+unittest_main()
+
+// --------
