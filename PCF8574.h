e016a04863c506229420c377f1078ef16ad57bcb
mattbue <mattbue@users.noreply.github.com>
2021-01-03 14:43:38 +0100

    My implementation to additionally support Wire1 to Wire5

--- PCF8574.h
+++ PCF8574.h
@@ -3,7 +3,7 @@
 //    FILE: PCF8574.H
 //  AUTHOR: Rob Tillaart
 //    DATE: 02-febr-2013
-// VERSION: 0.2.4
+// VERSION: 0.2.5
 // PURPOSE: Arduino library for PCF8574 - I2C IO expander
 //     URL: https://github.com/RobTillaart/PCF8574
 //          http://forum.arduino.cc/index.php?topic=184800
@@ -15,12 +15,12 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#define PCF8574_LIB_VERSION "0.2.4"
+#define PCF8574_LIB_VERSION "0.2.5"
 
-#define PCF8574_OK          			      0x00
+#define PCF8574_OK          		0x00
 #define PCF8574_INTERFACE_NOT_AVAILABLE 0x80
-#define PCF8574_PIN_ERROR   			      0x81
-#define PCF8574_I2C_ERROR   			      0x82
+#define PCF8574_PIN_ERROR   		0x81
+#define PCF8574_I2C_ERROR   		0x82
 
 
 enum{
