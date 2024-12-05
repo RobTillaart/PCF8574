08a3a2bfb9e09ce10b41033f9cae3dcae181d558
hasenradball <mail@frankhaefele.de>
2024-12-05 21:12:56 +0100

    document code parts only in header file

--- PCF8574.cpp
+++ PCF8574.cpp
@@ -11,23 +11,10 @@
 
 #include "PCF8574.h"
 
-/**
- * @brief Construct a new PCF8574::PCF8574 object
- * 
- * @param deviceAddress optional address of I2C device; default = 0x20;
- * @param wire optional address of Wire instance; default = \&Wire;
- */
 PCF8574::PCF8574(const uint8_t deviceAddress, TwoWire *wire)
 : _address {deviceAddress}, _wire {wire}
 {}
 
-/**
- * @brief init PCF8574 instance
- * 
- * @param value  optional init value to write; default = 0xFF;
- * @return true 
- * @return false 
- */
 bool PCF8574::begin(uint8_t value)
 {
   if (! isConnected()) return false;
@@ -35,36 +22,18 @@ bool PCF8574::begin(uint8_t value)
   return true;
 }
 
-/**
- * @brief check if device is connected
- * 
- * @return true 
- * @return false 
- */
 bool PCF8574::isConnected()
 {
   _wire->beginTransmission(_address);
   return ( _wire->endTransmission() == 0);
 }
 
-/**
- * @brief set address of PCF8574 device
- * 
- * @param deviceAddress I2C address; possible values 0x20...0x27
- * @return true 
- * @return false 
- */
 bool PCF8574::setAddress(const uint8_t deviceAddress)
 {
   _address = deviceAddress;
   return isConnected();
 }
 
-/**
- * @brief read all 8 bit at once from PCF8574
- * 
- * @return uint8_t value read from device
- */
 uint8_t PCF8574::read8()
 {
   if (_wire->requestFrom(_address, (uint8_t)1) != 1)
@@ -76,25 +45,6 @@ uint8_t PCF8574::read8()
   return _dataIn;
 }
 
-/**
- * @brief write all 8 bit at once to PCF8574 device
- * 
- * @param value to write
- */
-void PCF8574::write8(const uint8_t value)
-{
-  _dataOut = value;
-  _wire->beginTransmission(_address);
-  _wire->write(_dataOut);
-  _error = _wire->endTransmission();
-}
-
-/**
- * @brief read one bit from PCF8574 device
- * 
- * @param pin pin to read (0...7)
- * @return uint8_t value
- */
 uint8_t PCF8574::read(const uint8_t pin)
 {
   if (pin > 7)
@@ -106,12 +56,14 @@ uint8_t PCF8574::read(const uint8_t pin)
   return (_dataIn & (1 << pin)) > 0;
 }
 
-/**
- * @brief writes a single pin; pin = 0..7.
- * 
- * @param pin pin number for write (0...7)
- * @param value value to write, HIGH(1) or LOW (0)
- */
+void PCF8574::write8(const uint8_t value)
+{
+  _dataOut = value;
+  _wire->beginTransmission(_address);
+  _wire->write(_dataOut);
+  _error = _wire->endTransmission();
+}
+
 void PCF8574::write(const uint8_t pin, const uint8_t value)
 {
   if (pin > 7)
@@ -130,11 +82,6 @@ void PCF8574::write(const uint8_t pin, const uint8_t value)
   write8(_dataOut);
 }
 
-/**
- * @brief toggle one bit on PCF8574 device
- * 
- * @param pin pin number for write (0...7)
- */
 void PCF8574::toggle(const uint8_t pin)
 {
   if (pin > 7)
@@ -145,23 +92,12 @@ void PCF8574::toggle(const uint8_t pin)
   toggleMask(1 << pin);
 }
 
-/**
- * @brief toggles a selection of pins, if you want to invert all pins use 0xFF (default value).
- * 
- * @param mask 
- */
 void PCF8574::toggleMask(const uint8_t mask)
 {
   _dataOut ^= mask;
   PCF8574::write8(_dataOut);
 }
 
-/**
- * @brief shifts output channels n pins (default 1) pins right (e.g. LEDs ).
- * Fills the higher lines with zero's.
- * 
- * @param n number of shifts
- */
 void PCF8574::shiftRight(const uint8_t n)
 {
   if ((n == 0) || (_dataOut == 0)) return;
@@ -170,12 +106,6 @@ void PCF8574::shiftRight(const uint8_t n)
   PCF8574::write8(_dataOut);
 }
 
-/**
- * @brief shifts output channels n pins (default 1) pins left (e.g. LEDs ).
- * Fills the lower lines with zero's
- * 
- * @param n number of shifts
- */
 void PCF8574::shiftLeft(const uint8_t n)
 {
   if ((n == 0) || (_dataOut == 0)) return;
@@ -184,11 +114,6 @@ void PCF8574::shiftLeft(const uint8_t n)
   PCF8574::write8(_dataOut);
 }
 
-/**
- * @brief returns the last error from the lib (see .h file).
- * 
- * @return int error value to return
- */
 int PCF8574::lastError()
 {
   int e = _error;
@@ -196,12 +121,6 @@ int PCF8574::lastError()
   return e;
 }
 
-/**
- * @brief rotates output channels to right,
- * moving lowest line to highest line.
- * 
- * @param n number of moves
- */
 void PCF8574::rotateRight(const uint8_t n)
 {
   uint8_t r = n & 7;
@@ -210,22 +129,11 @@ void PCF8574::rotateRight(const uint8_t n)
   PCF8574::write8(_dataOut);
 }
 
-/**
- * @brief rotates output channels to left,
- * moving highest line to lowest line.
- * 
- * @param n number of moves
- */
 void PCF8574::rotateLeft(const uint8_t n)
 {
   rotateRight(8 - (n & 7));
 }
 
-/**
- * @brief reverse the "bit pattern" of the lines,
- * swapping pin 7 with 0, 6 with 1, 5 with 2 etc.
- * 
- */
 void PCF8574::reverse()  //  quite fast: 4 and, 14 shifts, 3 or, 3 assignment.
 {
   uint8_t x = _dataOut;
@@ -235,12 +143,6 @@ void PCF8574::reverse()  //  quite fast: 4 and, 14 shifts, 3 or, 3 assignment.
   PCF8574::write8(x);
 }
 
-/**
- * @brief read Button mask. 
- * 
- * @param mask use the mask set by setButtonMask to select specific input pins.
- * @return * uint8_t 
- */
 uint8_t PCF8574::readButton8(const uint8_t mask)
 {
   uint8_t temp = _dataOut;
@@ -250,12 +152,6 @@ uint8_t PCF8574::readButton8(const uint8_t mask)
   return _dataIn;
 }
 
-/**
- * @brief read button read a single input pin.
- * 
- * @param pin pin number to be read
- * @return uint8_t value read from pin
- */
 uint8_t PCF8574::readButton(const uint8_t pin)
 {
   if (pin > 7)
@@ -271,12 +167,6 @@ uint8_t PCF8574::readButton(const uint8_t pin)
   return value;
 }
 
-/**
- * @brief select sets the specified pin pin to High,
- * all others set to Low. If pin > 7 all pins are set to LOW.
- * 
- * @param pin pin which should be set to 1
- */
 void PCF8574::select(const uint8_t pin)
 {
   uint8_t n = 0x00;
@@ -284,12 +174,6 @@ void PCF8574::select(const uint8_t pin)
   write8(n);
 };
 
-/**
- * @brief select sets the pins from 0...pin to High,
- * all others set to Low. If pin > 7 all pins are set to LOW.
- * 
- * @param pin pin up to which should be set to 1
- */
 void PCF8574::selectN(const uint8_t pin)
 {
   uint8_t n = 0xFF;
@@ -297,6 +181,4 @@ void PCF8574::selectN(const uint8_t pin)
   write8(n);
 };
 
-
 //  -- END OF FILE --
-
--- PCF8574.h
+++ PCF8574.h
@@ -26,111 +26,236 @@
 #define PCF8574_I2C_ERROR           0x82
 
 
-class PCF8574
-{
-public:
-  explicit PCF8574(const uint8_t deviceAddress = 0x20, TwoWire *wire = &Wire);
+class PCF8574 {
+  public:
+    /**
+     * @brief Construct a new PCF8574::PCF8574 object
+     * 
+     * @param deviceAddress optional address of I2C device; default = 0x20;
+     * @param wire optional address of Wire instance; default = \&Wire;
+     */
+    explicit PCF8574(const uint8_t deviceAddress = 0x20, TwoWire *wire = &Wire);
 
-  bool    begin(uint8_t value = PCF8574_INITIAL_VALUE);
-  bool    isConnected();
-
-  //  note: setting the address corrupt internal buffer values
-  //  a read8() / write8() call updates them.
-  bool    setAddress(const uint8_t deviceAddress);
-
-  /**
-  * @brief getter for I2C address of pcf7485 device
-  *  should be in the range of 0x20...0x27
-  * 
-  * @return uint8_t address
-  */
-  uint8_t getAddress() const { return _address; }
-
-
-  uint8_t read8();
-  uint8_t read(const uint8_t pin);
+    /**
+     * @brief init PCF8574 instance
+     * 
+     * @param value  optional init value to write; default = 0xFF;
+     * @return true 
+     * @return false 
+     */
+    bool begin(uint8_t value = PCF8574_INITIAL_VALUE);
   
-  /**
-   * @brief returns the last read inputs again,
-   * as this information is buffered in the class this is faster than re-read the pins.
-   * 
-   * @return uint8_t internal member _dataIn
-   */
-  uint8_t value() const { return _dataIn; };
+    /**
+     * @brief check if device is connected
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool isConnected();
 
-  void    write8(const uint8_t value);
-  void    write(const uint8_t pin, const uint8_t value);
+    /**
+     * @brief set address of PCF8574 device
+     * note: setting the address corrupt internal buffer values.
+     * A read8() / write8() call updates them.
+     * @param deviceAddress I2C address; possible values 0x20...0x27
+     * @return true 
+     * @return false 
+     */
+    bool setAddress(const uint8_t deviceAddress);
+
+    /**
+    * @brief getter for I2C address of pcf7485 device
+    *  should be in the range of 0x20...0x27
+    * 
+    * @return uint8_t address
+    */
+    uint8_t getAddress() const { return _address; }
+
+    /**
+     * @brief read all 8 bit at once from PCF8574
+     * 
+     * @return uint8_t value read from device
+     */
+    uint8_t read8();
+
+    /**
+     * @brief read one bit from PCF8574 device
+     * 
+     * @param pin pin to read (0...7)
+     * @return uint8_t value
+     */
+    uint8_t read(const uint8_t pin);
+
+    /**
+     * @brief returns the last read inputs again,
+     * as this information is buffered in the class this is faster than re-read the pins.
+     * 
+     * @return uint8_t internal member _dataIn
+     */
+    uint8_t value() const { return _dataIn; };
+
+    /**
+     * @brief write all 8 bit at once to PCF8574 device
+     * 
+     * @param value to write
+     */
+    void write8(const uint8_t value);
+
+    /**
+     * @brief writes a single pin; pin = 0..7.
+     * 
+     * @param pin pin number for write (0...7)
+     * @param value value to write, HIGH(1) or LOW (0)
+     */
+    void write(const uint8_t pin, const uint8_t value);
+
+    /**
+     * @brief returns the last written data.
+     * 
+     * @return uint8_t internal member _dataOut
+     */
+    uint8_t valueOut() const { return _dataOut; }
+
+    /**
+     * @brief returns the set buttonMask.
+     * 
+     * @return uint8_t the mask for input lines
+     */
+    uint8_t readButton8() { return PCF8574::readButton8(_buttonMask); }
+
+    /**
+     * @brief read Button mask. 
+     * 
+     * @param mask use the mask set by setButtonMask to select specific input pins.
+     * @return * uint8_t 
+     */
+    uint8_t readButton8(const uint8_t mask);
+
+    /**
+     * @brief read button read a single input pin.
+     * 
+     * @param pin pin number to be read
+     * @return uint8_t value read from pin
+     */
+    uint8_t readButton(const uint8_t pin);
   
-  /**
-   * @brief returns the last written data.
-   * 
-   * @return uint8_t internal member _dataOut
-   */
-  uint8_t valueOut() const { return _dataOut; }
+    /**
+     * @brief sets the bit mask which lines are input.
+     * 
+     * @param mask bit mask of inputs
+     */
+    void setButtonMask(const uint8_t mask) { _buttonMask = mask; };
 
-  /**
-   * @brief returns the set buttonMask.
-   * 
-   * @return uint8_t the mask for input lines
-   */
-  uint8_t readButton8() { return PCF8574::readButton8(_buttonMask); }
-  
-  uint8_t readButton8(const uint8_t mask);
-  uint8_t readButton(const uint8_t pin);
-  
-  /**
-   * @brief sets the bit mask which lines are input.
-   * 
-   * @param mask bit mask of inputs
-   */
-  void    setButtonMask(const uint8_t mask) { _buttonMask = mask; };
+    /**
+     * @brief returns the internal button mask member
+     * 
+     * @return uint8_t the mask for input lines
+     */
+    uint8_t getButtonMask() { return _buttonMask; };
 
-  
-  /**
-   * @brief returns the internal button mask member
-   * 
-   * @return uint8_t the mask for input lines
-   */
-  uint8_t getButtonMask() { return _buttonMask; };
+    /**
+     * @brief toggle one bit on PCF8574 device
+     * 
+     * @param pin pin number for write (0...7)
+     */
+    void toggle(const uint8_t pin);
 
+    /**
+     * @brief toggles a selection of pins, if you want to invert all pins use 0xFF (default value).
+     * 
+     * @param mask 
+     */
+    void toggleMask(const uint8_t mask = 0xFF);
 
-  //  rotate, shift, toggle, reverse expect all lines are output
-  void    toggle(const uint8_t pin);
-  //      default 0xFF ==> invertAll()
-  void    toggleMask(const uint8_t mask = 0xFF);
-  void    shiftRight(const uint8_t n = 1);
-  void    shiftLeft(const uint8_t n = 1);
-  void    rotateRight(const uint8_t n = 1);
-  void    rotateLeft(const uint8_t n = 1);
-  void    reverse();
+    /**
+     * @brief shifts output channels n pins (default 1) pins right (e.g. LEDs ).
+     * Fills the higher lines with zero's.
+     * 
+     * @param n number of shifts
+     */
+    void shiftRight(const uint8_t n = 1);
 
-  void    select(const uint8_t pin);
-  void    selectN(const uint8_t pin);
+    /**
+     * @brief shifts output channels n pins (default 1) pins left (e.g. LEDs ).
+     * Fills the lower lines with zero's
+     * 
+     * @param n number of shifts
+     */
+    void shiftLeft(const uint8_t n = 1);
 
-  /**
-   * @brief sets all pins to LOW.
-   * 
-   */
-  void    selectNone() { write8(0x00); };
+    /**
+     * @brief rotates output channels to right,
+     * moving lowest line to highest line.
+     * 
+     * @param n number of moves
+     */
+    void rotateRight(const uint8_t n = 1);
 
-  /**
-   * @brief sets all pins to HIGH.
-   * 
-   */
-  void    selectAll()  { write8(0xFF); };
+    /**
+     * @brief rotates output channels to left,
+     * moving highest line to lowest line.
+     * 
+     * @param n number of moves
+     */
+    void rotateLeft(const uint8_t n = 1);
 
-  int     lastError();
+    /**
+     * @brief reverse the "bit pattern" of the lines,
+     * swapping pin 7 with 0, 6 with 1, 5 with 2 etc.
+     * 
+     */
+    void    reverse();
 
-private:
-  int     _error {PCF8574_OK};
-  uint8_t _address;
-  uint8_t _dataIn {0};
-  uint8_t _dataOut {0xFF};
-  uint8_t _buttonMask {0xFF};
-  
+    /**
+     * @brief select sets the specified pin pin to High,
+     * all others set to Low. If pin > 7 all pins are set to LOW.
+     * 
+     * @param pin pin which should be set to 1
+     */
+    void    select(const uint8_t pin);
 
-  TwoWire*  _wire;
-};
+    /**
+     * @brief select sets the pins from 0...pin to High,
+     * all others set to Low. If pin > 7 all pins are set to LOW.
+     * 
+     * @param pin pin up to which should be set to 1
+     */
+    void    selectN(const uint8_t pin);
+
+    /**
+     * @brief sets all pins to LOW.
+     * 
+     */
+    void selectNone() { write8(0x00); };
+
+    /**
+     * @brief sets all pins to HIGH.
+     * 
+     */
+    void selectAll() { write8(0xFF); };
+
+    /**
+     * @brief returns the last error from the lib (see .h file).
+     * 
+     * @return int error value to return
+     */
+    int lastError();
+
+  private:
+
+    // internal member for error
+    int _error {PCF8574_OK};
+    // internal buffer device address
+    uint8_t _address;
+    // internal buffer for data read from device
+    uint8_t _dataIn {0};
+    // internal buffer for data write to device
+    uint8_t _dataOut {0xFF};
+    // internal buffer for button (input) lines
+    uint8_t _buttonMask {0xFF};
+
+    TwoWire*  _wire;
+  };
 
 
 //  -- END OF FILE --
